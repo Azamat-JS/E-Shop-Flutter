841334bcd42abe347da291b683e2dbc651c48348
Azamat-JS <azamatielts987@gmail.com>
2026-05-28 19:45:11 +0500

    implemented themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:e_shop_flutter/constants/global_variables.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,9 +11,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'E-Shop',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: const Text('Flutter Demo Home Page'),
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(elevation: 0),
+        iconTheme: IconThemeData(color: Colors.black),
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text("E-Shop-App")),
+        body: const Center(child: Text('Flutter Demo Home Page')),
+      ),
     );
   }
 }
