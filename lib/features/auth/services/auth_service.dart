2a91ffb6ae380a4f0f08b44556cafe1ba2d9239c
Azamat-JS <azamatielts987@gmail.com>
2026-06-01 16:39:38 +0500

    completed signup function in auth service

--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -1,9 +1,13 @@
+import 'package:e_shop_flutter/constants/error_handling.dart';
 import 'package:e_shop_flutter/constants/global_variables.dart';
+import 'package:e_shop_flutter/constants/utils.dart';
 import 'package:e_shop_flutter/models/user.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class AuthService {
   void signUpUser({
+    required BuildContext context,
     required String email,
     required String password,
     required String name,
@@ -27,6 +31,18 @@ class AuthService {
         },
       );
       print(res.body);
-    } catch (e) {}
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSuccess: () {
+          showSnackbar(
+            context,
+            "Account created! Login with the same credentials.",
+          );
+        },
+      );
+    } catch (e) {
+      showSnackbar(context, e.toString());
+    }
   }
 }
