a3930e5d56bb0b31f276d2d72d5f107fd9993239
Azamat-JS <azamatielts987@gmail.com>
2026-06-03 19:09:06 +0500

    implemented top categories section in home screen

--- lib/features/home/widgets/address_box.dart
+++ lib/features/home/widgets/address_box.dart
@@ -1,10 +1,13 @@
+import 'package:e_shop_flutter/providers/user_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class AddressBox extends StatelessWidget {
   const AddressBox({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<UserProvider>(context).user;
     return Container(
       height: 40,
       decoration: BoxDecoration(
@@ -13,7 +16,28 @@ class AddressBox extends StatelessWidget {
             Color.fromARGB(255, 114, 226, 221),
             Color.fromARGB(255, 162, 236, 233),
           ],
+          stops: [0.5, 1],
         ),
+      ),
+      padding: EdgeInsets.only(left: 10),
+      child: Row(
+        children: [
+          const Icon(Icons.location_on_outlined, size: 20),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 5),
+              child: Text(
+                'Delivery to ${user.name} -  ${user.address}',
+                style: TextStyle(fontWeight: FontWeight.w500),
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsetsGeometry.only(left: 5, top: 2),
+            child: Icon(Icons.arrow_drop_down_outlined, size: 20),
+          ),
+        ],
       ),
     );
   }
