62544288d7d2fb27ead8c6e641a15405a58c2252
Azamat-JS <azamatielts987@gmail.com>
2026-06-16 19:26:40 +0500

    updated deal of day widget

--- lib/features/home/widgets/deal_of_day.dart
+++ lib/features/home/widgets/deal_of_day.dart
@@ -38,7 +38,7 @@ class _DealOfDayState extends State<DealOfDay> {
                 child: Text('Deal of the Day', style: TextStyle(fontSize: 20)),
               ),
               Image.network(
-                "https://images.unsplash.com/photo-1536782376847-5c9d14d97cc0?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZnJlZXxlbnwwfHwwfHx8MA%3D%3D",
+                product!.images[0],
                 height: 240,
                 fit: BoxFit.fitHeight,
               ),
