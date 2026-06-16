ee334c3300694bacd595bc5c93c729eef8eb57e3
Azamat-JS <azamatielts987@gmail.com>
2026-06-16 19:32:41 +0500

    implemented navigator route in deal of day section

--- lib/features/home/widgets/deal_of_day.dart
+++ lib/features/home/widgets/deal_of_day.dart
@@ -1,5 +1,7 @@
 import 'package:e_shop_flutter/common/widgets/loader.dart';
 import 'package:e_shop_flutter/features/home/services/home_services.dart';
+import 'package:e_shop_flutter/features/product_details/screens/product_details_screen.dart';
+import 'package:e_shop_flutter/features/product_details/services/product_details_services.dart';
 import 'package:e_shop_flutter/models/product.dart';
 import 'package:flutter/material.dart';
 
@@ -24,65 +26,79 @@ class _DealOfDayState extends State<DealOfDay> {
     setState(() {});
   }
 
+  void navigateToDetailScreen() {
+    Navigator.pushNamed(
+      context,
+      ProductDetailsScreen.routeName,
+      arguments: product,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return product == null
         ? Loader()
         : product!.name.isEmpty
         ? const SizedBox()
-        : Column(
-            children: [
-              Container(
-                alignment: Alignment.topLeft,
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Text('Deal of the Day', style: TextStyle(fontSize: 20)),
-              ),
-              Image.network(
-                product!.images[0],
-                height: 240,
-                fit: BoxFit.fitHeight,
-              ),
-              Container(
-                alignment: Alignment.topLeft,
-                padding: const EdgeInsets.only(left: 15),
-                child: const Text('\$100', style: TextStyle(fontSize: 18)),
-              ),
-              Container(
-                alignment: Alignment.topLeft,
-                padding: const EdgeInsets.only(left: 15, top: 5, right: 40),
-                child: Text(
-                  "Azamat",
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
+        : GestureDetector(
+            onTap: navigateToDetailScreen,
+            child: Column(
+              children: [
+                Container(
+                  alignment: Alignment.topLeft,
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Text(
+                    'Deal of the Day',
+                    style: TextStyle(fontSize: 20),
+                  ),
                 ),
-              ),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: product!.images
-                      .map(
-                        (e) => Image.network(
-                          e,
-                          height: 100,
-                          width: 100,
-                          fit: BoxFit.fitWidth,
-                        ),
-                      )
-                      .toList(),
+                Image.network(
+                  product!.images[0],
+                  height: 240,
+                  fit: BoxFit.fitHeight,
                 ),
-              ),
-              Container(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 15,
-                ).copyWith(left: 15).copyWith(left: 15),
-                alignment: Alignment.topLeft,
-                child: Text(
-                  'See all deals',
-                  style: TextStyle(color: Colors.cyan[800]),
+                Container(
+                  alignment: Alignment.topLeft,
+                  padding: const EdgeInsets.only(left: 15),
+                  child: const Text('\$100', style: TextStyle(fontSize: 18)),
                 ),
-              ),
-            ],
+                Container(
+                  alignment: Alignment.topLeft,
+                  padding: const EdgeInsets.only(left: 15, top: 5, right: 40),
+                  child: Text(
+                    "Azamat",
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: product!.images
+                        .map(
+                          (e) => Image.network(
+                            e,
+                            height: 100,
+                            width: 100,
+                            fit: BoxFit.fitWidth,
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 15,
+                  ).copyWith(left: 15).copyWith(left: 15),
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'See all deals',
+                    style: TextStyle(color: Colors.cyan[800]),
+                  ),
+                ),
+              ],
+            ),
           );
   }
 }
