75ee10d8197abd52eecd206b028cc3f10ba269ae
Azamat-JS <azamatielts987@gmail.com>
2026-06-24 19:39:51 +0500

    updated address screent

--- lib/features/address/screens/address_screen.dart
+++ lib/features/address/screens/address_screen.dart
@@ -1,5 +1,8 @@
+import 'package:e_shop_flutter/common/widgets/custom_textfield.dart';
 import 'package:e_shop_flutter/constants/global_variables.dart';
+import 'package:e_shop_flutter/providers/user_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class AddressScreen extends StatefulWidget {
   static const String routeName = '/address';
@@ -10,8 +13,24 @@ class AddressScreen extends StatefulWidget {
 }
 
 class _AddressScreenState extends State<AddressScreen> {
+  final _addressFormKey = GlobalKey<FormState>();
+  final TextEditingController _flatBuildingController = TextEditingController();
+  final TextEditingController _areaController = TextEditingController();
+  final TextEditingController _pincodeController = TextEditingController();
+  final TextEditingController _cityController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _flatBuildingController.dispose();
+    _areaController.dispose();
+    _pincodeController.dispose();
+    _cityController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    var address = 'Al-Beruniy 27 kv-1, Urgench, Khorezm. UZB';
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(60),
@@ -20,6 +39,61 @@ class _AddressScreenState extends State<AddressScreen> {
             decoration: const BoxDecoration(
               gradient: GlobalVariables.appBarGradient,
             ),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              if (address.isNotEmpty)
+                Column(
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black12),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          address,
+                          style: const TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              Form(
+                key: _addressFormKey,
+                child: Column(
+                  children: [
+                    CustomTextfield(
+                      controller: _flatBuildingController,
+                      hintText: "Flat, House No, Building",
+                    ),
+                    const SizedBox(height: 10),
+                    CustomTextfield(
+                      controller: _areaController,
+                      hintText: "Area, Street",
+                    ),
+                    const SizedBox(height: 10),
+                    CustomTextfield(
+                      controller: _pincodeController,
+                      hintText: "Pincode",
+                      obscure: true,
+                    ),
+                    const SizedBox(height: 10),
+                    CustomTextfield(
+                      controller: _cityController,
+                      hintText: "Town/City",
+                      obscure: true,
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
