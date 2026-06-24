3f77cf8d06fa8c418af13f066890ac6a18a778ba
Azamat-JS <azamatielts987@gmail.com>
2026-06-24 21:41:32 +0500

    implemented app pay and google pay buttons

--- lib/features/address/screens/address_screen.dart
+++ lib/features/address/screens/address_screen.dart
@@ -1,8 +1,8 @@
 import 'package:e_shop_flutter/common/widgets/custom_textfield.dart';
 import 'package:e_shop_flutter/constants/global_variables.dart';
-import 'package:e_shop_flutter/providers/user_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:pay/pay.dart';
 
 class AddressScreen extends StatefulWidget {
   static const String routeName = '/address';
@@ -19,6 +19,29 @@ class _AddressScreenState extends State<AddressScreen> {
   final TextEditingController _pincodeController = TextEditingController();
   final TextEditingController _cityController = TextEditingController();
 
+  List<PaymentItem> paymentItems = [];
+  PaymentConfiguration? _applePayConfig;
+  PaymentConfiguration? _googlePayConfig;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadPaymentConfig();
+    _loadGooglePayConfig();
+  }
+
+  Future<void> _loadPaymentConfig() async {
+    final jsonString = await rootBundle.loadString('assets/applepay.json');
+    final config = PaymentConfiguration.fromJsonString(jsonString);
+    setState(() => _applePayConfig = config);
+  }
+
+  Future<void> _loadGooglePayConfig() async {
+    final jsonString = await rootBundle.loadString('assets/gpay.json');
+    final config = PaymentConfiguration.fromJsonString(jsonString);
+    setState(() => _googlePayConfig = config);
+  }
+
   @override
   void dispose() {
     super.dispose();
@@ -27,6 +50,10 @@ class _AddressScreenState extends State<AddressScreen> {
     _pincodeController.dispose();
     _cityController.dispose();
   }
+
+  void onApplePayResult(res) {}
+
+  void onGooglePayResult(res) {}
 
   @override
   Widget build(BuildContext context) {
@@ -93,9 +120,32 @@ class _AddressScreenState extends State<AddressScreen> {
                       hintText: "Town/City",
                       obscure: true,
                     ),
+                    const SizedBox(height: 10),
                   ],
                 ),
               ),
+              if (_applePayConfig != null)
+                ApplePayButton(
+                  width: double.infinity,
+                  height: 50,
+                  margin: const EdgeInsets.only(top: 15),
+                  style: ApplePayButtonStyle.whiteOutline,
+                  type: ApplePayButtonType.buy,
+                  paymentConfiguration: _applePayConfig!,
+                  paymentItems: paymentItems,
+                  onPaymentResult: onApplePayResult,
+                ),
+              const SizedBox(height: 10),
+              if (_googlePayConfig != null)
+                GooglePayButton(
+                  width: double.infinity,
+                  height: 50,
+                  margin: const EdgeInsets.only(top: 15),
+                  type: GooglePayButtonType.buy,
+                  paymentConfiguration: _googlePayConfig!,
+                  paymentItems: paymentItems,
+                  onPaymentResult: onGooglePayResult,
+                ),
             ],
           ),
         ),
